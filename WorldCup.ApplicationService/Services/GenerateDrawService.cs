c64634f8003d9130be516dd05432a1681bcdca67
Dan Gavrila <dan.gavrila3@gmail.com>
2023-06-23 19:08:04 +0300

    Partial implementation of draw service.

--- WorldCup.ApplicationService/Services/GenerateDrawService.cs
+++ WorldCup.ApplicationService/Services/GenerateDrawService.cs
@@ -1,4 +1,6 @@
 ﻿using WorldCup.ApplicationService.Models;
+using WorldCup.DataAccess.Entities;
+using WorldCup.DataAccess.Repositories;
 
 namespace WorldCup.ApplicationService.Services
 {
@@ -9,9 +11,52 @@ namespace WorldCup.ApplicationService.Services
 
     public class GenerateDrawService : IGenerateDrawService
     {
-        public Task<DrawResult> DrawGroups(int groupCount, string firstName, string surname)
+        private readonly WorldCupDbUoW _dbUoW;
+        public GenerateDrawService(WorldCupDbUoW worldCupDbUoW)
         {
-            return null;
+            _dbUoW = worldCupDbUoW ?? throw new ArgumentNullException(nameof(worldCupDbUoW));
+        }
+
+        public async Task<DrawResult> DrawGroups(int groupCount, string firstName, string surname)
+        {
+            var groupsDic = new Dictionary<int, DataAccess.Entities.Group>();
+            var userEntity = new User()
+            {
+                FirstName = firstName,
+                Surname = surname,
+            };
+            var currentDate = DateTime.Now;
+
+            using (_dbUoW)
+            {
+                var groups = await _dbUoW.GroupsRepository.GetAsync();
+                groupsDic = groups.ToDictionary(s => s.Id);
+
+                var teams = await _dbUoW.TeamsRepository.GetAsync();
+                var teamsArray = teams.ToArray();
+
+                int totalRounds = groupsDic.Count / groupCount;
+                int totalDraws = totalRounds * groupCount;
+
+                for (int i = 0; i < totalDraws; i++)
+                {
+                    var newDraw = new Draw()
+                    {
+                        CreatedOn = currentDate,
+                        User = userEntity,
+                        GroupsCount = groupCount,
+                        GroupId = i + 1,
+                    };
+
+                    _dbUoW.DrawsRepository.Insert(newDraw);
+                }
+
+                await _dbUoW.SaveAsync();
+            }
+            
+            //TODO: map Draw entity to DrawResult
+
+            return new DrawResult();
         }
     }
 }
