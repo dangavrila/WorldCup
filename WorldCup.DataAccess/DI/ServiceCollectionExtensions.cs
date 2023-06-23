942ce44984e28e8d18c301e81b7b8f7cc03406ea
Dan Gavrila <dan.gavrila3@gmail.com>
2023-06-23 21:42:22 +0300

    Fix bug in db context lifecycle.

--- WorldCup.DataAccess/DI/ServiceCollectionExtensions.cs
+++ WorldCup.DataAccess/DI/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@ namespace WorldCup.DataAccess.DI
         public static IServiceCollection AddDataAccessModule(this IServiceCollection services)
         {
             services.AddDbContext<WorldCupDbContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
-            services.AddSingleton<WorldCupDbUoW>();
+            services.AddScoped<WorldCupDbUoW>();
             return services;
         }
     }
