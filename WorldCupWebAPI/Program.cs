8a6287594e082c953e6d83def0baf45045f08382
Dan Gavrila <dan.gavrila3@gmail.com>
2023-06-26 11:53:58 +0300

    Configure web API behavior for model validationd and configure error page for nod-dev envs.

--- WorldCupWebAPI/Program.cs
+++ WorldCupWebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using WorldCup.ApplicationService.DI;
 using WorldCup.WebAPI.Filters;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WorldCup.WebAPI
 {
@@ -14,7 +17,20 @@ namespace WorldCup.WebAPI
             builder.Services.AddControllers(options =>
             {
                 options.Filters.Add<NotImplementexExFilter>();
-            });
+            })
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = (context) =>
+                    new BadRequestObjectResult(context.ModelState)
+                    {
+                        ContentTypes =
+                        {
+                            Application.Json,
+                            Application.Xml
+                        }
+                    };
+            })
+            .AddXmlSerializerFormatters();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -27,6 +43,33 @@ namespace WorldCup.WebAPI
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler(exceptionHandlerApp =>
+                {
+                    exceptionHandlerApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        context.Response.ContentType = Text.Plain;
+
+                        await context.Response.WriteAsync("An exception was thrown.");
+
+                        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+                        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                        {
+                            await context.Response.WriteAsync(" The file was not found.");
+                        }
+
+                        if (exceptionHandlerPathFeature?.Path == "/")
+                        {
+                            await context.Response.WriteAsync(" Page: Home.");
+                        }
+                    });
+                });
+
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
